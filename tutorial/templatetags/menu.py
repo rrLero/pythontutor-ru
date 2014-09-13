a123dcb26743b2000753a439903b10e36bfd26be
leenr <i@leenr.ru>
2014-09-14 03:00:13 +1200

    Fixed: navbar item Lessons was not active in the problem page

--- tutorial/templatetags/menu.py
+++ tutorial/templatetags/menu.py
@@ -6,20 +6,20 @@ from django.template import Node
 register = template.Library()
 
 class IfPageIsNode(Node):
-    def __init__(self, if_page, nodelist):
-        self.if_page = if_page
+    def __init__(self, if_pages, nodelist):
+        self.if_pages = if_pages
         self.nodelist = nodelist
 
     def render(self, context):
         try:
             resolved = urlresolvers.resolve(context.get('request').path)
-            return self.nodelist.render(context) if resolved.url_name == self.if_page else ''
+            return self.nodelist.render(context) if resolved.url_name in self.if_pages else ''
         except:
             return []
 
 @register.tag
 def ifpageis(parser, token):
-    tag_name, if_page = token.split_contents()
+    tag_name, *if_pages = token.split_contents()
     nodelist = parser.parse(('endif',))
     parser.delete_first_token()
-    return IfPageIsNode(if_page, nodelist)
+    return IfPageIsNode(if_pages, nodelist)
